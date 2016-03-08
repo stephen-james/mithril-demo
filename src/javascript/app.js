dcefcd981ae5ba44a0b99b790dd44273a00f1f34
stephen-james <stephenjames@me.com>
2016-03-08 22:33:12 +0000

    Code Slide 2b - two way binding, lacking mount

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -5,7 +5,16 @@ var firstName = m.prop('Stephen');
 
 var view = m('div.greeter', [
   m('label', 'Enter your name'),
-  m('input', { value: firstName() })
+  m('input', {
+    value: firstName(),
+    oninput: m.withAttr('value', firstName)
+  }),
+  m('div', 'Hello ' + firstName() + '!'),
+  m('button', { onclick: showNameValue }, 'Show Current Value')
 ]);
 
 m.render(target, view);
+
+function showNameValue() {
+  alert('Current value of firstName: ' + firstName());
+}
